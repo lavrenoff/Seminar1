c692dba989944689db281e208762e8675ba0dcf8
Лавренов Евгений <lavr020775@gmail.com>
2022-08-19 01:50:29 +0300

    zadacha8

---
+++ zadacha8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число=");
+int number1=1;
+int number2= Convert.ToInt32(Console.ReadLine());
+
+
+while(number1<=number2)
+ {
+ if(number1%2==0)
+    Console.WriteLine(number1);
+
+ number1++;
+ }
