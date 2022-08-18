8119450151998f14e579cd140f8260058a366ca3
Лавренов Евгений <lavr020775@gmail.com>
2022-08-19 01:18:34 +0300

    Домашнее задание 2

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите 1 число=");
+int numberA= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число=");
+int numberB= Convert.ToInt32(Console.ReadLine());
+int imax=0;
+
+if(numberA>numberB)
+   imax=numberA;
+   else
+   imax=numberB;
+   
+
+Console.WriteLine("max = "+imax);
